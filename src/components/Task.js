25a4b71e881dffc54b545c4e2c8cd7470294f506
Kirill Begey <kirill.begey@mail.ru>
2024-04-06 17:24:15 +0300

    fix: исправить компонент Task

--- src/components/Task.js
+++ src/components/Task.js
@@ -23,16 +23,16 @@ export default class Task {
       this._editTask(this._taskElement.querySelector('.tasks__item-title').textContent, this._taskElement);
     });
     this._taskElement.querySelector('#buttonRemove').addEventListener('click', () => {
-      this._deleteTask(this._text, this._taskElement);
+      this._deleteTask(this._taskElement.querySelector('.tasks__item-title').textContent, this._taskElement);
     });
     if (this._taskElement.querySelector('#buttonProgress')) {
       this._taskElement.querySelector('#buttonProgress').addEventListener('click', () => {
-        this._inProgressTask(this._text, this._taskElement);
+        this._inProgressTask(this._taskElement.querySelector('.tasks__item-title').textContent, this._taskElement);
       });
     }
     if (this._taskElement.querySelector('#buttonDone')) {
       this._taskElement.querySelector('#buttonDone').addEventListener('click', () => {
-        this._doneTask(this._text, this._taskElement);
+        this._doneTask(this._taskElement.querySelector('.tasks__item-title').textContent, this._taskElement);
       });
     }
   }
